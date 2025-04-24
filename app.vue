4682fabdd3c75e375c2013d916234bd807b2a509
thammakorn <thammakorn.s@ku.th>
2025-04-25 00:50:42 +0700

    Changes_Images_Icon

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@ useHead({
     {
       name: "description",
       content:
-        "Nuxt Starter is a minimal template created for quick landing pages, built with Nuxt and Tailwind CSS.",
+        "We’re not your typical data science company. We build web tools that tackle the messy, overlooked, and just plain annoying parts of data work—like missing values, clunky prep, and the pain of trial-and-error modeling. Our tools aren’t trying to be everything. They’re sharp, focused, and built for folks who’d rather spend time thinking than cleaning.",
     },
   ],
   bodyAttrs: {
