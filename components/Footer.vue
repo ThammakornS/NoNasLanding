b6e66dfb3fad9ec0585377d300f8bee81bf7cf83
thammakorn <thammakorn.s@ku.th>
2025-04-27 02:02:08 +0700

    Update link & Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="my-20">
     <p class="text-sm text-center text-zinc-600">
-      Copyright &copy; {{ new Date().getFullYear() }} Nuxt Starter. Powered by <a href="https://www.netlify.com/" class="underline">Netlify</a>.
+      Copyright &copy; {{ new Date().getFullYear() }} Indie Data Science. Powered by <a href="https://www.netlify.com/" class="underline">Netlify</a> & <a href="https://github.com/nuxt/starter" class="underline">Nuxt Starter</a>.
     </p>
   </footer>
 </template>
