2d2326cfa866acccc28799d2e7cfd2a4f1008ad3
thammakorn <thammakorn.s@ku.th>
2025-04-26 19:15:39 +0700

    Browse button edit

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,8 @@ definePageMeta({
 
 <template>
   <Container>
+    <!-- Hero section -->
+    <section>
     <Hero
       :title="page.hero.title"
       :description="page.hero.description"
@@ -16,21 +18,39 @@ definePageMeta({
       :imageAlt="page.hero.imageAlt"
       :buttons="page.hero.buttons"
     ></Hero>
+    </section>
+
+    <!-- Logos section with id -->
+    <section>
     <Logos :title="page.logos.title" :icons="page.logos.icons"></Logos>
+    </section>
+
+    <!-- Features section with id -->
+    <section id="features">
     <Features
       :title="page.features.title"
       :description="page.features.description"
       :items="page.features.items"
     ></Features>
+    </section>
+
+    <!-- Testimonials section with id -->
+    <section>
     <Testimonials
       :title="page.testimonials.title"
       :description="page.testimonials.description"
       :items="page.testimonials.items"
     ></Testimonials>
+    </section>
+
+    <!-- Cta section with id -->
+    <section>
     <Cta
       :title="page.cta.title"
       :description="page.cta.description"
       :buttons="page.cta.buttons"
     ></Cta>
+    </section>
+
   </Container>
 </template>
